883ad3e73c53fe06c0222d12dbe4be5d2945e692
Yaroslav Korshak <ykorshak@gmail.com>
2024-11-24 22:18:15 +0100

    improves variable naming and exception handling

--- app/routes.js
+++ app/routes.js
@@ -3,28 +3,29 @@ const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 
 // Route to retrieve a user by their ID.
-router.get('/user/:userid', (req, res) => {
+router.get('/user/:userId', (req, res) => {
   const db = new sqlite3.Database(req.app.get('config').DATABASE);
-  const userid = req.params.userid;
+  const userId = req.params.userId;
 
   // Validate that a user ID is provided.
-  if (!userid) {
+  if (!userId) {
     res.status(400).json({ error: 'User ID is required' });
     return;
   }
 
   // Execute an SQL query to fetch the user from the database.
-  db.get("SELECT * FROM users WHERE id = ?", [userid], (err, row) => {
+  db.get('SELECT * FORM users WHERE id = ?', [userId], (err, user) => {
     db.close();
 
     // Handle potential errors during the database query.
     if (err) {
-      return res.status(500).json({ error: 'Database error' });
+      res.status(500).json({ error: 'Database error' }); //Generic error response to client
+      return;
     }
 
     // Check if a user with the given ID was found.
-    if (row) {
-      res.json({ id: row.id, name: row.name, email: row.email });
+    if (user) {
+      res.json({ id: user.id, name: user.name, email: user.email });
     } else {
       res.status(404).json({ message: 'User not found' });
     }
@@ -34,10 +35,16 @@ router.get('/user/:userid', (req, res) => {
 // Route to create a new user.
 router.post('/user/', (req, res) => {
   const db = new sqlite3.Database(req.app.get('config').DATABASE);
-  const data = req.body;
+  const { name, email } = req.body;
+
+  // Input validation
+  if (!name || !email) {
+    res.status(400).json({ error: 'Name and email are required' });
+    return;
+  }
 
   // Execute an SQL query to insert a new user into the database.
-  db.run("INSERT INTO users (name, email) VALUES (?, ?)", [data.name, data.email], function(err) {
+  db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], function(err) {
     db.close();
 
     // Handle potential errors during the database insertion.
@@ -46,8 +53,7 @@ router.post('/user/', (req, res) => {
     }
 
     // Return a 201 Created response with the ID of the newly created user.
-    // this.lastID is a property of the statement object, containing the ID of the last inserted row.
-    res.status(201).json({ message: 'User created', id: this.lastID }); 
+    res.status(201).json({ message: 'User created', id: this.lastID });
   });
 });
 
